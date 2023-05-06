f2ed852b1d4bc0c82a6721b33928e64c5cce4f3a
Hamza Yaseen <hamza.yaseen@healthwire.pk>
2023-05-07 00:47:31 +0500

    Appointment search verification 5 steps done

---
+++ features/appointments_search.feature
@@ -0,0 +1,8 @@
+Feature: Appointments Search
+
+  Scenario: Doctor Search Verification
+    Given I click on doctor Search
+    When I search the doctor by name
+    Then I verify that the searched doctor is populated or not
+    Then I clicked on the searched doctor
+    Then I verify the recently selected doctor
---
+++ features/step_definitions/appointments_search_steps.rb
@@ -0,0 +1,40 @@
+Given(/^I click on doctor Search$/) do
+  bookdoctor = @wait.until { @driver.find_element(:xpath, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageView[1]') }
+  bookdoctor.click
+  sleep(5)
+  search_bar = @driver.find_element(:id, 'com.healthwire.healthwire:id/tvSearchBar')
+  search_bar_text = search_bar.text
+  expect('Search for doctor or specialties here').to eq(search_bar_text)
+  search_bar.click
+end
+
+When(/^I search the doctor by name$/) do
+  doctor = ['Assist. Prof. Dr. Mujahid Israr', 'Dr. Asif Mehmood', 'Dr. Bilal Bin Mukhtar', 'Dr. Salman Javed',
+            'Dr. Rabbia Ashraf', 'Dr. Fauzia Anjum', 'Dr. Kamran Ashraf Khan', 'Dr. Ali Kazmi', 'Dr. Sana Younas']
+  @doctor_name = doctor.sample
+  search_doctors = @wait.until { @driver.find_element(:id, 'com.healthwire.healthwire:id/etDoctorSearch') }
+  search_doctors.send_keys @doctor_name
+  @driver.hide_keyboard
+end
+
+Then(/^I verify that the searched doctor is populated or not$/) do
+  available_doctor = @driver.find_element(:id, 'com.healthwire.healthwire:id/rvSearchedDoctor')
+  @searched_doctor = available_doctor.find_element(:id, 'com.healthwire.healthwire:id/constDoctors')
+  @searched_doctor_name = @searched_doctor.find_element(:id, 'com.healthwire.healthwire:id/tvGeneralPhysicianDrName').text
+  expect(@searched_doctor_name).to eq(@doctor_name)
+end
+
+Then(/^I clicked on the searched doctor$/) do
+  @searched_doctor.click
+end
+
+Then(/^I verify the recently selected doctor$/) do
+  2.times do
+    @driver.navigate.back
+  end  
+  recent_search = @wait.until { @driver.find_element(:id, 'com.healthwire.healthwire:id/rvRecentSearch') }
+  recent_doctor = recent_search.find_elements(:id, 'com.healthwire.healthwire:id/constRecentSearch')[0]
+  card_speciality = recent_doctor.find_element(:id, 'com.healthwire.healthwire:id/cardSpeciality')
+  card_doctor_name = card_speciality.find_element(:id, 'com.healthwire.healthwire:id/tvGeneralPhysicianDrName').text
+  expect(card_doctor_name).to eq(@searched_doctor_name)
+end
